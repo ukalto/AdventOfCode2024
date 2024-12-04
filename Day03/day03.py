046c6a1f63fcd4e48fb1db9a88ac27872a70ed5b
ukalto <maxi.gaber@gmail.com>
2024-12-04 11:18:18 +0100

    day03

---
+++ Day03/day03.py
@@ -0,0 +1,17 @@
+import re
+
+
+def solve_first():
+    with open('day03.txt', 'r') as file:
+        print(sum([int(x) * int(y) for x, y in re.findall(r"mul\((\d{1,3}),\s?(\d{1,3})\)", ''.join([line.strip() for line in file.readlines()]))]))
+
+
+def solve_second():
+    with open('day03.txt', 'r') as file:
+        content = ''.join(line.strip() for line in file).split("don't()")
+        dos = [content[0]] + [do.split("do()")[1:] for idx, do in enumerate(content[1:], start=1)]
+        print(sum([int(x) * int(y) for x, y in re.findall(r"mul\((\d{1,3}),\s?(\d{1,3})\)", ''.join([str(do) for do in dos]))]))
+
+if __name__ == '__main__':
+    solve_first()
+    solve_second()
